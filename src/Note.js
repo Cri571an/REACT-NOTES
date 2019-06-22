d5178a04e740ae5fc086f33c0d8667dee280989d
Cristian <cristianantonioespinoza@hotmail.com>
2019-06-22 14:33:01 -0500

    Tercer commit agregando inputs 6/22/19

--- src/Note.js
+++ src/Note.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const Note = props => {
-  console.log(props);
+  const note = props.notes.filter( note => note.id === parseInt (props.match.params.id))[0];
   return(
     <React.Fragment>
       <Typography align='center' variant='h4' gutterBottom>
-      Title
+      {note.title}
       </Typography>
-      <Typography variant='subtitle1'>Description</Typography>
+      <Typography variant='subtitle1'>{note.description}</Typography>
     </React.Fragment>
   )
 }
